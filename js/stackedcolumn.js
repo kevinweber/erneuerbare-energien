1a4d9f8827f669e19b67935a7f5f89f7890634b2
Patrick Frank <main_pfrank@freenet.de>
2015-01-21 10:01:57 +0100

    Theme angepasst unfertig, Renew Reihenfolge for Brenn

--- js/stackedcolumn.js
+++ js/stackedcolumn.js
@@ -5,7 +5,7 @@ for (x in energy){
 }
 
 var chart_data_type = 1;
-var energy_production_types = ["Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie","Sonnenenergie","Biomasse und Abfälle","Geothermische Energie","Wasserkraft","Windkraft"];
+var energy_production_types = ["Sonnenenergie","Biomasse und Abfälle","Geothermische Energie","Wasserkraft","Windkraft","Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie"];
 var energy_consume_types = ["Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie","Erneuerbare Energien"];
 var energy_axis_text = "";
 
@@ -16,15 +16,15 @@ if (chart_data_type == 1){
 	}
 
 	for (x in energy){
-		series[0].data.push(energy[x].production_coal_absolute);
-		series[1].data.push(energy[x].production_oil_absolute);
-		series[2].data.push(energy[x].production_gas_absolute);
-		series[3].data.push(energy[x].production_core_absolute);
-		series[4].data.push(energy[x].production_renewable_solar_absolute);
-		series[5].data.push(energy[x].production_renewable_waste_absolute);
-		series[6].data.push(energy[x].production_renewable_geothermic_absolute);
-		series[7].data.push(energy[x].production_renewable_water_absolute);
-		series[8].data.push(energy[x].production_renewable_wind_absolute);		
+		series[0].data.push(energy[x].production_renewable_solar_absolute);
+		series[1].data.push(energy[x].production_renewable_waste_absolute);
+		series[2].data.push(energy[x].production_renewable_geothermic_absolute);
+		series[3].data.push(energy[x].production_renewable_water_absolute);
+		series[4].data.push(energy[x].production_renewable_wind_absolute);	
+		series[5].data.push(energy[x].production_coal_absolute);
+		series[6].data.push(energy[x].production_oil_absolute);
+		series[7].data.push(energy[x].production_gas_absolute);
+		series[8].data.push(energy[x].production_core_absolute);	
 	}
 	energy_axis_text = "Primärerzeugnis in Tonnen Rohöleinheit";
 }
@@ -78,7 +78,7 @@ $(function () {
             column: {
                 stacking: 'normal',
                 dataLabels: {
-                    enabled: true,
+                    enabled: false,
                     color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
                     style: {
                         textShadow: '0 0 3px black'
@@ -90,3 +90,8 @@ $(function () {
         series: series
     });
 });
+
+Highcharts.theme = {
+    colors: ['#aace92','#a0c884','#96c276','#8bbc69','#81b65b','#000000','#000000','#000000','#000000']};
+			 
+Highcharts.setOptions(Highcharts.theme);
