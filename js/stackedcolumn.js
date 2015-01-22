0f7f4da6c429084d806d08dd44d8496766dd6fc6
Patrick Frank <main_pfrank@freenet.de>
2015-01-22 11:20:51 +0100

    Diverse Anpassungen, Button Grp um produktion und verbrauch zu switchen

--- js/stackedcolumn.js
+++ js/stackedcolumn.js
@@ -1,51 +1,50 @@
+var energy_production_types = ["Sonnenenergie","Biomasse und Abfälle","Geothermische Energie","Wasserkraft","Windkraft","Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie"];
+var energy_consume_types = ["Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie","Erneuerbare Energien"];
+
 var parse_xAxis = [];
 
 for (x in energy){
 	parse_xAxis.push(energy[x].name_de);
 }
 
-var chart_data_type = 1;
-var energy_production_types = ["Sonnenenergie","Biomasse und Abfälle","Geothermische Energie","Wasserkraft","Windkraft","Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie"];
-var energy_consume_types = ["Stein- und Braunkohle","Rohöl","Naturgas","Kernenergie","Erneuerbare Energien"];
 var energy_axis_text = "";
 
-var series = [];
-if (chart_data_type == 1){
-	for (x in energy_production_types){
-		series.push({name:energy_production_types[x],data:[],stack:"production"});
+function reloadHS(chart_type) {
+	var series = [];
+	
+	if(chart_type == 'produce'){
+		for (x in energy_production_types){
+			series.push({name:energy_production_types[x],data:[],stack:"production"});
+		}
+
+		for (x in energy){
+			series[0].data.push(energy[x].production_renewable_solar_absolute);
+			series[1].data.push(energy[x].production_renewable_waste_absolute);
+			series[2].data.push(energy[x].production_renewable_geothermic_absolute);
+			series[3].data.push(energy[x].production_renewable_water_absolute);
+			series[4].data.push(energy[x].production_renewable_wind_absolute);	
+			series[5].data.push(energy[x].production_coal_absolute);
+			series[6].data.push(energy[x].production_oil_absolute);
+			series[7].data.push(energy[x].production_gas_absolute);
+			series[8].data.push(energy[x].production_core_absolute);	
+		}
+		energy_axis_text = "Primärerzeugnis in Tonnen Rohöleinheit";
+	}
+	else if(chart_type == 'consume'){
+		for (x in energy_consume_types){
+			series.push({name:energy_consume_types[x],data:[],stack:"consume"});
+		}
+
+		for (x in energy){
+			series[0].data.push(energy[x].consume_coal_absolute);
+			series[1].data.push(energy[x].consume_oil_absolute);
+			series[2].data.push(energy[x].consume_gas_absolute);
+			series[3].data.push(energy[x].consume_core_absolute);
+			series[4].data.push(energy[x].consume_renewable_total_absolute);		
+		}
+		energy_axis_text = "Inländischer Bruttoenergieverbrauch in Tonnen Rohöleinheit";
 	}
 
-	for (x in energy){
-		series[0].data.push(energy[x].production_renewable_solar_absolute);
-		series[1].data.push(energy[x].production_renewable_waste_absolute);
-		series[2].data.push(energy[x].production_renewable_geothermic_absolute);
-		series[3].data.push(energy[x].production_renewable_water_absolute);
-		series[4].data.push(energy[x].production_renewable_wind_absolute);	
-		series[5].data.push(energy[x].production_coal_absolute);
-		series[6].data.push(energy[x].production_oil_absolute);
-		series[7].data.push(energy[x].production_gas_absolute);
-		series[8].data.push(energy[x].production_core_absolute);	
-	}
-	energy_axis_text = "Primärerzeugnis in Tonnen Rohöleinheit";
-}
-else {
-	for (x in energy_consume_types){
-		series.push({name:energy_consume_types[x],data:[],stack:"consume"});
-	}
-
-	for (x in energy){
-		series[0].data.push(energy[x].consume_coal_absolute);
-		series[1].data.push(energy[x].consume_oil_absolute);
-		series[2].data.push(energy[x].consume_gas_absolute);
-		series[3].data.push(energy[x].consume_core_absolute);
-		series[4].data.push(energy[x].consume_renewable_total_absolute);		
-	}
-	energy_axis_text = "Inländischer Bruttoenergieverbrauch in Tonnen Rohöleinheit";
-}
-console.log(series);
-//reset array: series.length = 0;
-
-$(function () {
     $('#container_pf').highcharts({
 
         chart: {
@@ -89,11 +88,14 @@ $(function () {
 
         series: series
     });
-});
+};
+
 
 Highcharts.theme = {
     colors: ['#146196','#5195c4','#629fc9','#73a9cf','#84b3d4','#b4481f','#b9562f','#be6440','#c47251']};
 	
-	//Grün #a0c884
+	//sunburst ['#146196','#5195c4','#629fc9','#73a9cf','#84b3d4','#b4481f','#b9562f','#be6440','#c47251']
 			 
 Highcharts.setOptions(Highcharts.theme);
+
+$(reloadHS('produce'));
